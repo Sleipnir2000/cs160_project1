3603d54b5684a358314be08ec79e1594d2f30c82
Sleipnir2000 <45472221+Sleipnir2000@users.noreply.github.com>
2022-09-26 15:51:20 -0700

    Delete print.cpp

--- print.cpp
+++
@@ -1,9 +0,0 @@
-#include "print.hpp"
-
-// The visitNode function for this visitor first prints the
-// node then recursively visits the children. This leads to
-// a preorder traversal/print of the tree.
-void PrintVisitor::visitNode(Node* node){
-	std::cout<<node->value<<" ";
-	node->visit_children(this);
-}
